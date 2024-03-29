87b4a7d530cbf15681be8f7ff3b2c717531a26d8
Mustafa250IITJ <m22aie250@iitj.ac.in>
2024-03-29 22:28:51 +0530

    Activity3 load resnet50 model in version1.0

--- loadmodel.py
+++ loadmodel.py
@@ -2,4 +2,6 @@ import torch
 import torchvision.models as models
 
 # download ResNet model
-resnet101 = models.resnet101(pretrained=True)
+# resnet101 = models.resnet101(pretrained=True)
+resnet50 = models.resnet50(pretrained=True)
+
