8c20aad68fe1ac8e191c993df8b8695a2776b280
Mustafa250IITJ <m22aie250@iitj.ac.in>
2024-03-30 14:06:30 +0530

    Final1 commit

--- rollbf.py
+++ rollbf.py
@@ -1,6 +1,9 @@
 import torch
-import torchvision.models as models
-
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms, models
+import numpy as np
+import matplotlib.pyplot as plt
 # download ResNet model
 resnet50 = models.resnet50(pretrained=True)
 resnet50.eval()
